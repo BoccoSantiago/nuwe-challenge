96ab425639683807a6d607b222546535d474d7d4
BoccoSantiago <italobocco@gmail.com>
2022-07-16 13:44:34 +0200

    update

--- repte-3/src/components/LoginPage.js
+++ repte-3/src/components/LoginPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import loginImg from '../img/login.png'
 
 
 
 export default function LogInPage() {
     return (
-        <div className='body'>
+        <div className='container-login'>
+            <div className='inputs'>
             <h2>Welcome Back!</h2>
             <form>
                 <p>
@@ -20,12 +22,14 @@ export default function LogInPage() {
                 </p>
                 <br/>
                 <p>
-                    <button id="sub_btn" type="submit">Login</button>
+                    <button className="sub-btn" type="submit">Login</button>
                 </p>
             </form>
             <footer>
                 <p>Don't have and account? <Link to="/register">Register</Link>.</p>
             </footer>
+            </div>
+            <img className='loginImg' alt="login" src={loginImg}/>
         </div>
     )
 }
